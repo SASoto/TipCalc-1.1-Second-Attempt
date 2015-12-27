dde82727a9ef745305bb4b174b97fd4281ffc7f7
Saul Soto <SASoto@sauls-mbp.home>
2015-12-27 17:03:15 -0500

    First commit. 1.1 copy of Xcode Project Tip Calculator.

---
+++ TipTap 1.1/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  TipTap 1.1
+//
+//  Created by Saul Soto on 12/15/15.
+//  Copyright © 2015 CodePath - Saul Soto. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaulttipcontrol: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(0.2, forKey: "default_tip_percentage")
+        defaults.synchronize()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+
+}
--- TipTap 1.1/ViewController.swift
+++ TipTap 1.1/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var billfield: UITextField!
+    @IBOutlet weak var tiplabel: UILabel!
+    @IBOutlet weak var totallabel: UILabel!
+    
+    @IBOutlet weak var tipcontrol: UISegmentedControl!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        tiplabel.text = "$0.00"
+        totallabel.text = "$0.00"
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func OnEditingChanged(sender: AnyObject)
+    {
+        var tippercent = [0.18, 0.2, 0.22]
+        var tippercentspec = tippercent[tipcontrol.selectedSegmentIndex]
+        
+        var billamount = NSString(string: billfield.text!).doubleValue
+        var tip = billamount * tippercentspec
+        var total = billamount + tip
+        
+        tiplabel.text = "\(tip)"
+        totallabel.text = "\(total)"
+        
+        tiplabel.text = String(format: "$%.2f", tip)
+        totallabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    @IBAction func ontap(sender: AnyObject)
+    {
+        view.endEditing(true)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tippercent = defaults.doubleForKey("default_tip_percentage")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
 
 }
 
